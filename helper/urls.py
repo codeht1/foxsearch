72c4ac9698b34ee402d1114796a89f2b6698a1d6
Praveen Yadav <praveenyadav1193@yahoo.com>
2014-10-26 23:53:16 +0530

    Changing admin

--- helper/urls.py
+++ helper/urls.py
@@ -19,5 +19,5 @@ urlpatterns = patterns('',
     #url(r'^.*$','searchreqs.views.home', name='home')
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^djangoadmindev1/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
